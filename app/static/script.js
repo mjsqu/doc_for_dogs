ad0ff0a56e33f44164edb6cd841767903510f0d6
Mark Johnston <mj2.squ@gmail.com>
2023-12-16 22:55:30 +1300

    Filtering on dogsAllowed in prep for leaflet mapping

--- app/static/script.js
+++ app/static/script.js
@@ -15,29 +15,48 @@
  
 // [START gae_python38_log]
 // [START gae_python3_log]
-'use strict';
 
-window.addEventListener('load', function () {
+// TODO: Set up a leaflet map here, centred on somewhere in the top-of-the-south
 
-  console.log("Hello World!");
-  
-  function getTracks() {
-    fetch('/track_filter', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
+// We have a selection box on the main page which allows you to pick which 'dogsAllowed' value
+// you want to get tracks for (e.g. Dogs on a leash only)        
+const sb = document.querySelector('#dogsel')
+
+// This sets up an event for when the selection box changes
+sb.onchange = (event) => {
+            event.preventDefault();
+            // The value chosen is added to the dogValue variable
+            var dogValue = sb[sb.selectedIndex].value;
+            // The dogValue variable goes into the payload for the internal API
+            var payload = {
               'key': 'dogsAllowed',
-              'value': 'Dogs allowed. Keep dog under control at all times.'
-            })
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-          }
-)};
-getTracks();
-});
+              'value': dogValue
+            };
+            // This fetch queries the internal API to find tracks using the payload above
+            // to see how this works, go to main.py and look for the track_filter route
+            fetch('/track_filter', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(payload)
+          })
+          .then((response) => response.json())
+          .then((data) => {
+              console.log(data);
+              var disp = document.getElementById('display');
+              var strout = [];
+              data.sort((a,b) => b.lat - a.lat)
+              for (track of data) {
+                // TODO: This line could be removed and replaced with a colour scheme or some other way of identifying which tracks are bikeable
+                if (track.permittedActivities.includes('Mountain biking')) {
+                  // TODO: This loop returns lat and lon for each track - set up leaflet markers for each point and add them to our map
+                  strout.push(`${track['name']} ${track['lat']},${track['lon']} ${track.region}`);
+                }
+              }
+              disp.innerHTML = `<pre>${strout.join("\r\n")}</pre>`;
+              
+          })
+        };
 // [END gae_python3_log]
 // [END gae_python38_log]
